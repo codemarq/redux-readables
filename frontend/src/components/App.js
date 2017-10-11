ef905c0524e77b02699765ac1ef55ad9c3f090e6
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-10-10 22:40:17 -0400

    CHORE: some JSX structure and api call

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import Header from './Header'
 import Post from './Post'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backend: 'backend-data'
+    }
+  }
+
+  componentDidMount() {
+    const url = `${process.env.REACT_APP_BACKEND}/categories`;
+    console.log('fetching from url', url);
+    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
+                 credentials: 'include' } )
+      .then( (res) => { return(res.text()) })
+      .then((data) => {
+        this.setState({backend:data});
+      });
+  }
+
+
   render() {
     return (
       <div className='App'>
-        <Header />
-        <Post />
+        <Route exact path='/' render={() => (
+          <div>
+          <p>{this.state.backend}</p>
+            <Header 
+              categories={this.state}
+            />
+            <Post />
+          </div>
+        )}/>
       </div>
     );
   }
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,21 +3,25 @@ import React, { Component } from 'react'
 class Post extends Component {
   render() {
     return (
-      <div className="col s12 m7">
-        <div className="card horizontal blue-grey darken-1">
-          <div className='card-content white-text'>
-            <span><h3>voteScore</h3></span>
-            <span><h4>voteScore</h4></span>
-          </div>
-          <div className="card-stacked">
-            <div className="card-content white-text">
-              <h3 className="header white-text">Post.title</h3>
-              <h4 className='header white-text'>Post.author</h4>
-              <p>Post.body</p>
-            </div>
-            <div className="card-action">
-              <button className='btn'><i className='material-icons'>comment</i></button>
-              <button className='btn'><i className='material-icons'>exposure_plus_1</i></button>
+      <div className='container'>
+        <div className='row'>
+          <div className="col s12">
+            <div className="card horizontal blue-grey darken-2">
+              <div className='card-content white-text'>
+                <span><h4>voteScore</h4></span>
+                <span><h5>voteScore</h5></span>
+              </div>
+              <div className="card-stacked">
+                <div className="card-content white-text">
+                  <h4 className="header white-text">Post.title</h4>
+                  <h5 className='header white-text'>Post.author</h5>
+                  <p>Post.body</p>
+                </div>
+                <div className="card-action">
+                  <button className='btn blue-grey darken-1'><i className='material-icons'>comment</i></button>
+                  <button className='btn blue-grey darken-1'><i className='material-icons'>exposure_plus_1</i></button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
