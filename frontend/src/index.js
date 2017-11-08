653a5df6c35d9369b84747e1c2754bc57bae24f6
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-11-07 20:48:30 -0500

    CHORE: created store, passed to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
+console.log(store)
 
 ReactDOM.render(
   <BrowserRouter><App store={store}/></BrowserRouter>,
